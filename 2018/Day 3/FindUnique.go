996a687476a0a2b1133b530d2eecc661b643b9a0
czfloyd <floyd.c23@gmail.com>
2018-12-03 01:09:06 -0600

    Day 3 Part 2

--- 2018/Day 3/FindUnique.go
+++ 2018/Day 3/FindUnique.go
@@ -55,11 +55,33 @@ func main()  {
 		}
 	}
 
-	for _,l := range m {
-		for _,v := range l {
-			if v > 1 {
-				out++
+	for _, element := range s {
+		var unique = true
+
+		var n [5]int
+		var words = strings.Fields(element)
+		num1,_ := strconv.Atoi(words[0][1:])
+		n[0] = num1
+		var pos = strings.Split(words[2], ",")
+		num2,_ := strconv.Atoi(pos[0])
+		n[1] = num2
+		num3,_ := strconv.Atoi(pos[1][:len(pos[1]) - 1])
+		n[2] = num3
+		var size = strings.Split(words[3], "x")
+		num4,_ := strconv.Atoi(size[0])
+		n[3] = num4
+		num5,_ := strconv.Atoi(size[1])
+		n[4] = num5
+		for i := n[1]; i < n[1] + n[3] && unique; i++ {
+			for j := n[2]; j < n[2] + n[4]; j++ {
+				if m[i][j] > 1 {
+					unique = false
+					break
+				}
 			}
+		}
+		if unique {
+			fmt.Printf("%d\n",n[0])
 		}
 	}
 
