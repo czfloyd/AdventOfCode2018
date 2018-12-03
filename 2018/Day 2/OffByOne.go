3f2a2726bf38e0537d918647d1bc2bbf446006c4
czfloyd <floyd.c23@gmail.com>
2018-12-02 23:45:05 -0600

    Day 2 Part 2

---
+++ 2018/Day 2/OffByOne.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+    "fmt"
+	"os"
+)
+
+func main()  {
+	file,err := os.Open("input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer file.Close()
+
+	var s []string
+	var out []rune
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		s = append(s, scanner.Text())
+	}
+	
+	for _, e1 := range s {
+		for _, e2 := range s {
+			if offByOne(e1, e2) {
+				fmt.Printf("%s %s\n", e1, e2)
+				for i, r := range e1 {
+					if e1[i] == e2[i] {
+						out = append(out, r)
+					}
+				}
+			}
+		}
+	}
+	fmt.Printf("%s",out)
+}
+
+func offByOne(w1 string, w2 string) bool {
+	if w1 == w2{
+		return false
+	}
+	var m int = 1
+	
+	for i,_ := range w1 {
+		if w1[i] != w2[i] {
+			m--;
+			if m < 0 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
