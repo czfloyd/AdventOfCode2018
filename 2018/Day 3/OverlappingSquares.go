9f34cb8c880cfabef89bf5b538215deab50b8ba0
czfloyd <floyd.c23@gmail.com>
2018-12-03 00:43:13 -0600

    Day 3 Part 1

---
+++ 2018/Day 3/OverlappingSquares.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+    "fmt"
+	"os"
+	"strings"
+	"strconv"
+)
+
+func main()  {
+	file,err := os.Open("input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer file.Close()
+
+	var s []string
+	var m map[int]map[int]int
+	m = make(map[int]map[int]int)
+	var out int = 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		s = append(s, scanner.Text())
+	}
+
+	
+	for _, element := range s {
+		var n [5]int
+		var words = strings.Fields(element)
+		num1,_ := strconv.Atoi(words[0][1:])
+		n[0] = num1
+		var pos = strings.Split(words[2], ",")
+		num2,_ := strconv.Atoi(pos[0])
+		n[1] = num2
+		num3,_ := strconv.Atoi(pos[1][:len(pos[1]) - 1])
+		n[2] = num3
+		var size = strings.Split(words[3], "x")
+		num4,_ := strconv.Atoi(size[0])
+		n[3] = num4
+		num5,_ := strconv.Atoi(size[1])
+		n[4] = num5
+		for i := n[1]; i < n[1] + n[3]; i++ {
+			if (m[i] == nil){
+				m[i] = make(map[int]int)
+			}
+			for j := n[2]; j < n[2] + n[4]; j++ {
+				if m[i][j] >= 0 {
+					m[i][j]++
+				}else{
+					m[i][j] = 1
+				}
+			}
+		}
+		//fmt.Printf("%d\n",n);
+	}
+
+	for _,l := range m {
+		for _,v := range l {
+			if v > 1 {
+				out++
+			}
+		}
+	}
+
+	fmt.Print(out)
+}
